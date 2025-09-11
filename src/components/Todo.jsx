b892acce79b39f39c5d404cec6f8b57158d86412
JaromiZsombi <jaromi.zsombi@gmail.com>
2025-09-11 10:50:36 +0200

    backend and frontend

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { todosData } from '../data'
+//import { todosData } from '../data'
 import { useState } from 'react';
 import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 import { IoCheckmarkDoneCircle } from 'react-icons/io5';
 import { FaTrash } from "react-icons/fa"
 import { NewTodo } from './NewTodo';
 import { TodoSummary } from './TodoSummary';
+import { useEffect } from 'react';
+import { addTodo, deleteAllTodo, deleteTodo, doneTodo, getTodos } from '../utils';
 
 export const Todo = () => {
-  const [todos, setTodos] = useState(todosData)
-  //console.log(todos);
-  console.log("renderelődik a todo")
+  const [todos, setTodos] = useState(null)
 
+  console.log(todos);
+
+  useEffect(() => {
+    getTodos(setTodos)
+  }, [])//az üres függőségi tömb azt jelzi hogy csak egyszer fut le a függvény amikor betöltődik az oldal
+
+  /*
   const handleDelete = (id) => {
     console.log(id)
     setTodos(prev => prev.filter(obj => obj.id != id))
@@ -36,17 +43,38 @@ export const Todo = () => {
     }
     setTodos(prev=>[...prev,newTodo])
   }
+  */
 
+  const handleDelete = async (id) => {
+    await deleteTodo(id)//az adatbázisban megtörténik a módosítás
+    getTodos(setTodos)
+  }
+
+  const handleAdd = async (descr) => {
+    await addTodo(descr)//az adatbázisban megtörténik a módosítás
+    getTodos(setTodos)
+  }
+
+
+  const handleDone = async (id) => {
+    await doneTodo(id)
+    getTodos(setTodos) 
+  }
+
+  const handleDeleteAll = async() => {
+    await deleteAllTodo()
+    getTodos(setTodos)
+  }
 
   return (
-    <div style={{maxWidth:"600px", padding:"1rem", margin:"auto", border:"1px solid red"}}>
-        <NewTodo handleAdd={handleAdd}/>
-        <Button onClick={handleDeleteAll}><FaTrash/></Button>
-      
+    <div style={{ maxWidth: "600px", padding: "1rem", margin: "auto", border: "1px solid red" }}>
+      <NewTodo handleAdd={handleAdd} />
+      <Button onClick={handleDeleteAll}><FaTrash /></Button>
+
       <ListGroup>
-        {todos.map(obj =>
+        {todos && todos.map(obj =>
           <ListGroupItem key={obj.id} className="d-flex justify-content-between">
-            <IoCheckmarkDoneCircle style={{ color: obj.isDone ? "green" : "red", fontSize: "2rem" }} onClick={()=>handleDone(obj.id)}/>
+            <IoCheckmarkDoneCircle style={{ color: obj.isDone ? "green" : "red", fontSize: "2rem" }} onClick={() => handleDone(obj.id)} />
             <div style={{ textDecoration: obj.isDone ? "line-through" : "none", }}>
               {obj.descr}
             </div>
@@ -55,8 +83,8 @@ export const Todo = () => {
         )}
 
       </ListGroup>
-      <TodoSummary todos={todos}/>
-  
+      {/*<TodoSummary todos={todos}/>*/}
+
     </div>
   )
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,31 @@
 export const randNr=(min, max)=>{
     return Math.floor(Math.random()*(max-min+1))+min
 }
+
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:8000/'
+
+export const getTodos= async (setTodos)=>{
+    const resp = await axios.get(baseUrl+"todos")
+    setTodos(resp.data)
+}
+//todo törlése
+export const deleteTodo = async (id) =>{
+    await axios.delete(`${baseUrl}todos/${id}`)
+}
+
+//új hozzáadása
+export const addTodo = async (descr)=>{
+    await axios.post(baseUrl+"todos/", {descr})
+}
+
+//HandleDone
+export const doneTodo = async (id)=>{
+    await axios.patch(`${baseUrl}todos/${id}`)
+}
+
+//todo összes törlése
+export const deleteAllTodo = async ()=>{
+    await axios.delete(`${baseUrl}delall`)
+}
